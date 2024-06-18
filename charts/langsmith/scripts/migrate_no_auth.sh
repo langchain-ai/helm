6d3cf033db63beb6aba2fdb1bef5963bf0d9487b
infra <mukil@langchain.dev>
2024-06-18 13:22:02 -0400

    chore: bump 0.6.7 images

--- charts/langsmith/scripts/migrate_no_auth.sh
+++ charts/langsmith/scripts/migrate_no_auth.sh
@@ -12,7 +12,8 @@ admin_user_email=$2
 
 #PostgreSQL query
 update_query_string=$(cat <<EOF
-UPDATE public.tenants SET display_name = 'Default', config = config || '{"is_personal": false, "max_identities": 100, "max_workspaces": -1}' where id='00000000-0000-0000-0000-000000000000';
+UPDATE public.tenants SET display_name = 'Default', config = config || '{"is_personal": false, "max_identities": 100}' where id='00000000-0000-0000-0000-000000000000';
+UPDATE public.organizations SET display_name = 'Default', is_personal = false, config = config || '{"max_identities": 100, "max_workspaces": -1}' where id=(SELECT organization_id FROM tenants WHERE id = '00000000-0000-0000-0000-000000000000');
 WITH admin_user_id AS (
     SELECT id FROM public.users WHERE email = '$admin_user_email'
 ),
